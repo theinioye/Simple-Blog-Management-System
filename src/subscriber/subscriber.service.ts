028591dba55c5269300be577f2205ab00c74bfe6
Inioluwa Oyetunji <theinioye@gmail.com>
2024-11-23 23:59:45 +0100

    services commit

--- src/subscriber/subscriber.service.ts
+++ src/subscriber/subscriber.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSubscriberDto } from './dto/create-subscriber.dto';
 import { UpdateSubscriberDto } from './dto/update-subscriber.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Subscriber } from './entities/subscriber.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SubscriberService {
-  create(createSubscriberDto: CreateSubscriberDto) {
-    return 'This action adds a new subscriber';
+  constructor(
+    @InjectRepository(Subscriber)
+    private subscriberRepository: Repository<Subscriber>,
+  ) {}
+
+  async create(createSubscriberDto: CreateSubscriberDto) {
+    return await this.subscriberRepository.save(createSubscriberDto);
   }
 
-  findAll() {
-    return `This action returns all subscriber`;
+  async findAll() {
+    return await this.subscriberRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} subscriber`;
+  async findOne(id: number) {
+    return await this.subscriberRepository.findOneBy({ id });
   }
 
   update(id: number, updateSubscriberDto: UpdateSubscriberDto) {
     return `This action updates a #${id} subscriber`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} subscriber`;
+  async remove(id: number) {
+    return await this.subscriberRepository.delete(id);
   }
 }
